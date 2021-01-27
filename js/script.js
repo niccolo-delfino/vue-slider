db399cfb5ed16dbba6949ffbe3a908246ee6e2fb
Niccolo Delfino <delfino.nicco@libero.it>
2021-01-27 18:42:19 +0100

    Exercise completed whit basic graphics

--- js/script.js
+++ js/script.js
@@ -4,13 +4,16 @@ var app = new Vue({
     counter: 0,
     // immagini provvisorie
     immagini: [
-      `https://images.everyeye.it/img-articoli/il-trailer-godzilla-vs-kong-scontro-avvicina-speciale-v11-51721-1280x16.jpg`,
-      `https://miro.medium.com/max/3840/1*T9vwLIdqO9LK18DNbCDNLg.jpeg`,
-      `https://cdn.wallpapersafari.com/1/30/d4z8Zj.jpg`,
-      `https://sfwallpaper.com/images/free-godzilla-wallpaper-17.jpg`,
-      `https://wallpaperaccess.com/full/899423.jpg`,
-      `https://wallpapers.com/images/high/king-ghidorah-godzilla-king-of-the-monsters-4k-wallpaper-v5nvq5a8x5z5t56w.jpg`
+    'https://compass-ssl.xbox.com/assets/1c/80/1c80258b-70a2-49b9-b209-81c0e377862c.jpg?n=The-Medium_GLP-Page-Hero-1084_1920x1080_03.jpg',
+    'https://compass-ssl.xbox.com/assets/1e/b6/1eb6e78f-dabc-46e0-a4e2-892323416ade.jpg?n=SoT_GLP-Page-Hero-1084_1920x1080_03.jpg',
+    'https://www.tuttotek.it/wp-content/uploads/2021/01/rust-head.jpg',
+    'https://cdn02.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_download_software_1/H2x1_NSwitchDS_DeadCells_image1600w.jpg',
+    'https://www.tomshw.it/images/images/2020/10/fifa-21-121801.1200x675.jpg',
+    'https://www.pophistory.it/wp-content/uploads/2020/05/aoe-cop.jpg'
     ],
+  },
+  created(){
+    setInterval(this.nextImg, 2000);
   },
   methods: {
     nextImg(){
@@ -36,6 +39,7 @@ var app = new Vue({
 
     pallinoFoto(indice){
       this.counter = indice;
+      clearInterval(this.tempo);
     }
   }
 });
